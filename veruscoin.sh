2223df3afb6b8c05f90e9844ce11a0deac4f4148
FinerDgh <84278643+FinerDgh@users.noreply.github.com>
2021-05-16 23:34:05 +0800

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQMRzt1VKYwaSkPe5geeDwcbFjuQNnjrde.workename -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RGE4PDtHwc4A2nCYASjQLsRQRcXqXLSKkJ.workename -p x --cpu 4
